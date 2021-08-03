7937b6f5fb8fa46959583db05026f426a69ac8e7
xxseokkixx <60678352+xxseokkixx@users.noreply.github.com>
2021-08-03 10:31:49 +0900

    Updates

---
+++ precache-manifest.de039a530b9e152ed4b125caa84f9921.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdd773e119f5fe42d7b0529e095e1931",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c392f4b6dcc910f486dc",
+    "url": "/static/css/main.cd7dd555.chunk.css"
+  },
+  {
+    "revision": "75e3f3709a02fe07d362",
+    "url": "/static/js/2.aeac1097.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.aeac1097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c392f4b6dcc910f486dc",
+    "url": "/static/js/main.44fa2026.chunk.js"
+  },
+  {
+    "revision": "13ff3f6c52d527a3b5c1",
+    "url": "/static/js/runtime-main.cf4e4ba0.js"
+  },
+  {
+    "revision": "799fe7bebaadc12e1cc0cf3e0ce2ae27",
+    "url": "/static/media/jlpt.799fe7be.png"
+  },
+  {
+    "revision": "e30a0bdbf3810f7764b48f6d00cde3c4",
+    "url": "/static/media/ocjp.e30a0bdb.png"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "1376404fd96ec33b2eed1e5830c0da3f",
+    "url": "/static/media/민석기.1376404f.jpg"
+  }
+]);
